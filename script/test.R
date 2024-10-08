de65e8d73cdde7cb9aa29a042fd137e02a01c69d
Luan Augusto <luanguto87@gmail.com>
2024-10-08 14:09:16 -0300

    Clone Information

---
+++ script/test.R
@@ -0,0 +1,10 @@
+remove.packages("sinasc")
+devtools::install()
+library(sinasc)
+devtools::check()
+
+db <- download.sinasc(inicio =  2023,fim = 2023,"CE",cod_mat = 2481286)
+meac <- label.sinasc(db)
+
+dbsp <- download.sinasc(2022,2022,"SP")
+SP <- label.sinasc(dbsp)
