150e5784834f27015e6eda0281da43612735afea
Luan Augusto <luanguto87@gmail.com>
2024-10-15 16:02:08 -0300

    Update test

--- script/test.R
+++ script/test.R
@@ -1,6 +1,7 @@
 remove.packages("sinasc")
 devtools::install()
-library(sinasc)
+library(PaNasc)
+?download.sinasc
 devtools::check()
 
 db <- download.sinasc(inicio =  2023,fim = 2023,"CE",cod_mat = 2481286)
