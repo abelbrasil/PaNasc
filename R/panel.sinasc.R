244c32f58c40b30f150480e8e0ce91fc4a758ef7
Luan Augusto <luanguto87@gmail.com>
2024-11-14 11:23:24 -0300

    create new function

--- R/panel.sinasc.R
+++ R/panel.sinasc.R
@@ -4,4 +4,12 @@ panel.sinasc <- function(x){
     select(CODESTAB,ESTABELECIMENTO,`LOCAL DE NASCIMENTO`=LOCNASC,SEXO,PESO,`FAIXA DE PESO`=FAIXA_PESO,`GESTAÇÃO`=GESTACAO,GRAVIDEZ,PARTO,DTNASC,HORANASC,ANO,`MES-ANO`=MES_ANO,`RAÇA/COR`=RACACOR,`CONSULTAS PRÉ-NATAL`=CONSULTAS,
            `IDADE DA MÃE`= IDADEMAE,`ESTADO CIVIL DA MÃE` = ESTCIVMAE,`RAÇA/COR DA MÃE`=RACACORMAE, `ESCOLARIDADE DA MÃE` = ESCMAE2010, `ANOS DE ESCOLARIDADE DA MÃE` = ESCMAE,`MUNICÍPIO RESIDENCIA MÃE`=MUNICIPIO,`OCUPAÇÃO DA MÃE`=OCUPMAE,
            `ANOMALIA CONGÊNITA`=IDANOMAL)
+
+  colnames(base) <- iconv(colnames(base), to = "UTF-8")
+
+  base <- lapply(dataset, function(col) {
+    if (is.character(col)) iconv(col, to = "UTF-8") else col
+  })
+
+  return(base)
 }
