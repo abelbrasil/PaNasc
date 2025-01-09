40beaab5b8f8bb316a3d6dfb2fba23e1fe06eaf7
Luan Augusto <luanguto87@gmail.com>
2025-01-09 15:28:34 -0300

    update external data

--- tests/download_dictonary.R
+++ tests/download_dictonary.R
@@ -27,6 +27,16 @@ unzip(temp,files = c("CBO2002 - Ocupacao.csv"),exdir = temp_dir)
 ocup <- read.csv2(paste0(temp_dir,"\\CBO2002 - Ocupacao.csv"))
 ocup$TITULO <- iconv(ocup$TITULO, from = "latin1", to = "UTF-8")
 
-usethis::use_data(CADMUN,CID10,TABUF,ocup, overwrite = T)
+CADMUN <- data.frame(lapply(CADMUN, function(x) if (is.factor(x)) as.character(x) else x))
+
+TABUF <- data.frame(lapply(TABUF, function(x) if (is.factor(x)) as.character(x) else x))
+
+CADMUN$MUNCOD <- as.integer(CADMUN$MUNCOD)
+
+TABUF$CODIGO <- as.integer(TABUF$CODIGO)
+
+usethis::use_data(CID10,ocup, overwrite = T)
+
+usethis::use_data(CADMUN,TABUF, overwrite = T)
 
 usethis::use_data(hospital_universitario_ebserh, overwrite = T)
