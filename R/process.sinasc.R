0f5650c99079d9e3a8c9bd7d7962a184f3855a4f
luan-augusto05 <luanguto87@gmail.com>
2025-07-19 10:30:01 -0300

    Update process.sinasc.R

--- R/process.sinasc.R
+++ R/process.sinasc.R
@@ -15,8 +15,53 @@ process.sinasc <- function(data){
   require(dplyr)
   require(lubridate)
   base <- data
+  
+  base <- base %>%
+    mutate(
+      DTNASC=dmy(DTNASC),
 
+      DTNASCMAE=dmy(DTNASCMAE),
 
+      DTCADASTRO=dmy(DTCADASTRO),
+
+      DTRECORIGA=dmy(DTRECORIGA),
+
+      DTRECEBIM=dmy(DTRECEBIM),
+
+      DTDECLARAC=dmy(DTDECLARAC),
+
+      DTULTMENST=dmy(DTULTMENST),
+
+      HORANASC=paste0(substr(HORANASC,1,2),":", substr(HORANASC,3,4)),
+
+      IDADEMAE=as.numeric(as.character(IDADEMAE)),
+
+      QTDFILVIVO=as.numeric(as.character(QTDFILVIVO)),
+
+      QTDFILMORT=as.numeric(as.character(QTDFILMORT)),
+
+      PESO=as.numeric(as.character(PESO)),
+
+      QTDGESTANT=as.numeric(as.character(QTDGESTANT)),
+
+      QTDPARTNOR=as.numeric(as.character(QTDPARTNOR)),
+
+      QTDPARTCES=as.numeric(as.character(QTDPARTCES)),
+
+      CONSPRENAT=as.numeric(as.character(CONSPRENAT)),
+
+      DIFDATA=as.numeric(as.character(DIFDATA)),
+
+      CONSPRENAT=as.numeric(as.character(CONSPRENAT)),
+
+      SERIESCMAE=as.numeric(as.character(SERIESCMAE)),
+
+      NUMEROLOTE=as.numeric(as.character(NUMEROLOTE)),
+
+      SEMAGESTAC=as.numeric(as.character(SEMAGESTAC)))
+
+  base <- base %>%
+    left_join(ESTAB%>% select(CNES, FANTASIA),by=join_by(CODESTAB==CNES),keep = F)  
   base <- base %>%
     mutate(ESTCIVMAE=recode(ESTCIVMAE,`1`="Solteira",`2`="Casada",`3`="Viúva",`4`="Divorciada",`5`="União Estável",`9`="Ignorado"),
 
